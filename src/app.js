48274eb8c1324f062fbf2584e8df0a77a32a3e8f
chathuras.12"cse.mrt.ac.lk <Cata"1993>
2019-07-21 10:25:24 +0800

    topics-server: new topic

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
+"use strict";
+const Topic= require( "./model/topic");
 const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
+
 io.on("connection", (socket) => {
+    let topicMap = {};
     let previousId;
     const safeJoin = (currentId) => {
         socket.leave(previousId);
@@ -15,14 +19,29 @@ io.on("connection", (socket) => {
     //     console.log(`Socket ${socket.id} has disconnected`);
     // });
 
-    socket.on("getTopic", (docId) => {
+    socket.on("newTopic", (topicId) => {
         console.log("get topic 4444");
-        safeJoin(docId);
-        socket.emit("topics", "gsdgdsg");
+        safeJoin(topicId);
+        let topic = new Topic(topicId,0);
+        topicMap[JSON.stringify(topicMap)]= topic;
+        console.log(topic);
+        console.log(topicMap);
+        socket.emit("topics", JSON.stringify(topicMap));
+        let str = JSON.stringify(topicMap);
+        console.log(str);
+        socket.emit("topics", str );
+        io.emit("topics", JSON.stringify(topicMap));
     });
 
-    socket.emit("topics", "ggggggggggg");
+    socket.on("getTopic", (topicId) => {
+        console.log("get topic 4444");
+        safeJoin(topicId);
+        socket.emit("topics", JSON.stringify(topicMap));
+    });
+
+    socket.emit("topics", JSON.stringify(topicMap));
     console.log(`Socket ${socket.id} has connected`);
+    let topic = new Topic('daa',0);
 });
 http.listen(4444, () => {
     console.log("Listening on port 4444");
