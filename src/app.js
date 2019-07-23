dd718fd5bc61b6fedfca38b29131e8475485bf89
chathuras.12"cse.mrt.ac.lk <Cata"1993>
2019-07-23 23:52:19 +0800

    topics-server: code cleanup

--- src/app.js
+++ src/app.js
@@ -6,42 +6,38 @@ const io = require("socket.io")(http);
 
 
 io.on("connection", (socket) => {
-    let topicMap = {};
-    let previousId;
-    const safeJoin = (currentId) => {
-        socket.leave(previousId);
-        socket.join(currentId, () => console.log(`Socket ${socket.id} joined room ${currentId}`));
-        previousId = currentId;
-    };
-
-    //
-    // io.on('disconnect', socket => {
-    //     console.log(`Socket ${socket.id} has disconnected`);
-    // });
 
     socket.on("newTopic", (topicId) => {
-        console.log("get topic 4444");
-        safeJoin(topicId);
+        console.log("new topic ",topicId);
         let topic = new Topic(topicId,0);
-        topicMap[JSON.stringify(topicMap)]= topic;
-        console.log(topic);
-        console.log(topicMap);
-        socket.emit("topics", JSON.stringify(topicMap));
-        let str = JSON.stringify(topicMap);
-        console.log(str);
-        socket.emit("topics", str );
+        topicMap[topicId]= topic;
         io.emit("topics", JSON.stringify(topicMap));
     });
 
     socket.on("getTopic", (topicId) => {
-        console.log("get topic 4444");
-        safeJoin(topicId);
+        console.log("get topics ");
         socket.emit("topics", JSON.stringify(topicMap));
+    });
+
+    socket.on("upvote", (topicId) => {
+        console.log("upvote topic ",topicId);
+        if(topicMap[topicId]) {
+            topicMap[topicId].upVote();
+        }
+
+        io.emit("topics", JSON.stringify(topicMap));
+    });
+
+    socket.on("downvote", (topicId) => {
+        console.log("downvote topic ",topicId)
+        if(topicMap[topicId]) {
+            topicMap[topicId].downVote();
+        }
+        io.emit("topics", JSON.stringify(topicMap));
     });
 
     socket.emit("topics", JSON.stringify(topicMap));
     console.log(`Socket ${socket.id} has connected`);
-    let topic = new Topic('daa',0);
 });
 http.listen(4444, () => {
     console.log("Listening on port 4444");
